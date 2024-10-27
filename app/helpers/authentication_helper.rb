2c294c4d90a4a0213b2ef61913b401bcb6801ae8
Ashwini Bala Ganesun <ashwinibalaganesun@gmail.com>
2024-10-27 14:10:22 -0400

    auth changes

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -1,12 +1,14 @@
 module AuthenticationHelper
   def encode_token(user_id)
     payload = { user_id: user_id, exp: 24.hours.from_now.to_i }
-    secret = Rails.application.credentials[:jwt_secret] || ENV["JWT_SECRET"] || "default_secret_key" # Fallback to an environment variable or default
+    # secret = Rails.application.credentials[:jwt_secret] || ENV["JWT_SECRET"] || "default_secret_key"
+    secret = "ce7bac9a8fc03a557603c9c55584baa0e5634b120cb47a95e78789e8bd1583a96d05893fffe810d1f921eaa36b4b85bb660a1f8703514f2b08d57a0943e70d47"
     JWT.encode(payload, secret, "HS256")
   end
 
   def decode_token(token)
-    secret = Rails.application.credentials[:jwt_secret] || ENV["JWT_SECRET"] || "default_secret_key" # Fallback to an environment variable or default
+    # secret = Rails.application.credentials[:jwt_secret] || ENV["JWT_SECRET"] || "default_secret_key"
+    secret = "ce7bac9a8fc03a557603c9c55584baa0e5634b120cb47a95e78789e8bd1583a96d05893fffe810d1f921eaa36b4b85bb660a1f8703514f2b08d57a0943e70d47"
     begin
       decoded_token = JWT.decode(token, secret, true, { algorithm: "HS256" })[0]
       decoded_token.symbolize_keys
